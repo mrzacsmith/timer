33017a302b27b9d042574cd0d252e28179051954
Zac Smith <zrs3141592@gmail.com>
2020-08-23 15:25:29 -0600

    create return time display

--- js/timer.js
+++ js/timer.js
@@ -1,10 +1,14 @@
 let countdown
 const displayCountdown = document.querySelector('.display_countdown')
+const endTime = document.querySelector('.display_return-time')
 
 const timer = (seconds) => {
+  clearInterval(countdown)
   const now = Date.now()
   const then = now + seconds * 1000
-  // console.log(now, then)
+  displayTimeLeft(seconds)
+  displayEndTime(then)
+
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000)
     if (secondsLeft <= 0) {
@@ -23,4 +27,16 @@ const displayTimeLeft = (seconds) => {
       : `${minutes}:0${secsRemaining}`
 
   displayCountdown.textContent = display
+}
+
+const displayEndTime = (timestamp) => {
+  const end = new Date(timestamp)
+  const hours = end.getHours()
+  const mins = end.getMinutes()
+  const adjustedHour = hours > 12 ? hours - 12 : hours
+  const adjustedMin = mins > 9 ? mins : `0${mins}`
+
+  const endTimeDisplay = `Be Back At ${adjustedHour}:${adjustedMin}`
+
+  endTime.textContent = endTimeDisplay
 }
